39105c7ed1ae7edf33a847bd04bbd7e17cec1bf6
yosf AL-otaibi <yousefalotaini21@gmail.com>
2024-11-11 20:26:32 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import "./App.css";
-import "./products";
-// import images from "./images.jpg";
-// import images2 from "./images2.jpg";
+
 function App() {
+  const products = [
+    {
+      name: "Apple Pie",
+      price: "15KD",
+      Image:
+        "https://schoolnightvegan.com/wp-content/uploads/2022/11/vegan-apple-pie-featured-1-2.jpg",
+    },
+    {
+      name: "Red Velvet",
+      price: "25KD",
+      Image:
+        "https://preppykitchen.com/wp-content/uploads/2022/07/Red-Velvet-Recipe-Card-1a.jpg",
+    },
+    {
+      name: "chocolate croissant",
+      price: "7KD",
+      Image:
+        "https://thecozyapron.com/wp-content/uploads/2024/02/easy-chocolate-croissants_thecozyapron_1.jpg",
+    },
+  ];
+
+  const productMenu = products.map((product, index) => {
+    return (
+      <div key={index}>
+        <img className="Product-Image" src={product.Image}></img>
+        <p>{product.name} </p>
+        <p>{product.price} </p>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
-      <h1>Sweetie Pies</h1>
+      <h1>Sweetie Bakery</h1>
       <p>Where Every Bite is an Event!</p>
       <img
         className="Bakery-Image"
         src="https://d2w1ef2ao9g8r9.cloudfront.net/otl-images/_1600x847_crop_center-center_82_line/open-a-bakery-header.jpg"
       ></img>
+      <div className="product-Menu">{productMenu}</div>
     </div>
   );
 }
